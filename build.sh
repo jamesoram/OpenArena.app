1f4c299df86c12994d8620bb98c9c4326be6d48e
Johannes Bittner <johannes.bittner@gmail.com>
2024-08-30 23:36:05 +0200

    apply duplicate-guid fix

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 cd "$(dirname "$0")"
 git clone https://github.com/ec-/Quake3e.git
 cd Quake3e
+git apply ../duplicate-guid-fix.patch
 make
 cp build/release-darwin-aarch64/libSDL2-2.0.0.dylib ..
 cp build/release-darwin-aarch64/*.aarch64 ..
